e6ea60cff20a11a501abbcfaa0e67ffe2d7900f5
moonok29 <aikomoon@naver.com>
2019-03-10 22:10:26 +0900

    Partial application 구현 및 사용법

---
+++ 02.Grammar/01.control.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------
+# 3항 연산자
+# ----------------------------------------------------
+
+def check_boolean(value):
+    if type(value) == bool:
+        return True
+    else:
+        return False
+
+def check_boolean_ternary_operator(value):
+    return True if type(value) == bool else False
+
+def main():
+    print("============= basic if/else")
+    print("Input boolean : %s" % check_boolean(True))
+    print("Input string : %s" % check_boolean("string"))
+
+    print("============ ternary operator")
+    print("Input boolean : %s" % check_boolean_ternary_operator(True))
+    print("Input string : %s" % check_boolean_ternary_operator("string"))
+
+if __name__ == "__main__":
+    main()
+
+
