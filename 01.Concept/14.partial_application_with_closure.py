09f9db0240eab9f72b8858d4b939484221744a38
moonok29 <aikomoon@naver.com>
2019-03-10 21:21:55 +0900

    인자 사용

---
+++ 01.Concept/14.partial_application_with_closure.py
@@ -0,0 +1,27 @@
+
+
+def partial(func, *a_args):
+
+    def wrapper(*b_args):
+        args = list(a_args)
+        args.extend(b_args)
+
+        return func(*args)
+
+    return wrapper
+
+def logging(year, month, day, title, content):
+    print("%s-%s-%s %s:%s" % (year,month, day, title, content))
+
+def main():
+    print("------------------ use logging function")
+    logging("2019","03","09","python2", "End of support in 2020")
+    logging("2019","03","09","python3", "Updateing")
+
+    print("------------------ use partial function")
+    f = partial(logging, "2019","03", "10")
+    f("python2", "End of support in 2020")
+    f("python3", "Updateing")
+
+if __name__ == "__main__":
+    main()
