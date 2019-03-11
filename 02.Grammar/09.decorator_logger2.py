1b5a8b637cb3f8ff45144cf89450ad74ee4d63ee
문정구 <moonok29@gmail.com>
2019-03-11 13:42:28 +0900

    코드 정리(공백추가)

--- 02.Grammar/09.decorator_logger2.py
+++ 02.Grammar/09.decorator_logger2.py
@@ -1,27 +1,27 @@
 import time
 import datetime
 
-def measure_run_time(func):
 
+def measure_run_time(func):
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
-        end  = time.time()
+        end = time.time()
 
-        print("%s, function running time : %s" % (func.__name__, end-start))
+        print("%s, function running time : %s" % (func.__name__, end - start))
         return result
 
     return wrapper
 
 
 def parameter_logger(func):
-
     def wrapper(*args, **kwargs):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H%M")
         print("[%s] args : %s, kwargs : %s" % (timestamp, args, kwargs))
         return func(*args, **kwargs)
 
     return wrapper
+
 
 def worker(delay_time):
     print("%d %s" % (delay_time, "초 후에 실행 됩니다."))
@@ -33,4 +33,3 @@ if __name__ == "__main__":
     f1 = parameter_logger(argument)
     f2 = measure_run_time(f1)
     f2(5)
-
